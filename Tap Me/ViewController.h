dc10154c4ddba01d66046411af267df16b50d12a
Cole Bratcher <colebratcher@The-Epi-Tome-2.local>
2014-01-14 15:54:32 -0800

    Initial Commit

---
+++ Tap Me/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by Cole Bratcher on 1/14/14.
+//  Copyright (c) 2014 Cole Bratcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Tap Me/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tap Me
+//
+//  Created by Cole Bratcher on 1/14/14.
+//  Copyright (c) 2014 Cole Bratcher. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
